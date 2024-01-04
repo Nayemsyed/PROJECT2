f181a6452391d66910f6c4ce5c6cbeeec219b7c0
Nayemsyed <sdnayem04112000@gmail.com>
2024-01-04 21:27:14 +0530

    Add project files.

---
+++ PROJECT2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader studentdetails = new StreamReader("D:\\Mphasis\\Project2\\Studentdetails.txt");
+                string str = studentdetails.ReadToEnd();
+                Console.WriteLine();
+                Console.WriteLine(str);
+                studentdetails.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
+
